c15cdf35b19f89c0e77cbf40dfa26a7eea7005ba
Jahnavi123-android <jahnavijaysinha123@gmail.com>
2025-07-30 11:12:26 +0530

    Delete Hotel data analysis.py

--- Hotel data analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-plt.show()
-df = pd.read_csv("C:\\Users\\jahna\\OneDrive\\Desktop\\Data analysis\\notes\\ADVANCE PYTHON\\tips.csv")  
-print(df.head())
-print(df.isnull().sum())
-print(df.tail(10))
